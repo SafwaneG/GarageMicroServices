5a0349b5e1c20419e12931b92b52f14ee062a630
elkadirizouhra <zahiraelkadiri6@gmail.com>
2024-10-27 21:29:53 +0100

    changes

--- vehicleService/src/main/java/com/garageSystem/vehicleService/service/VehicleService.java
+++ vehicleService/src/main/java/com/garageSystem/vehicleService/service/VehicleService.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class VehicleService {
     private final VehicleRepo vehicleRepo;
     public ResponseVehicleDTO createVehicle(RequestVehicleDTO requestVehicle){
-        VehicleModel vehicle= new VehicleModel(requestVehicle.ownerId(),requestVehicle.chassisNumber(),requestVehicle.chassisNumber(),requestVehicle.registrationNumber(),requestVehicle.brand(),requestVehicle.model(),requestVehicle.yearOfManufacture(),requestVehicle.color(),requestVehicle.mileage(),requestVehicle.fuelType(),requestVehicle.dateOfPurchase(),requestVehicle.vehicleCondition());
+        VehicleModel vehicle= new VehicleModel(requestVehicle.identityNumber(),requestVehicle.chassisNumber(),requestVehicle.chassisNumber(),requestVehicle.registrationNumber(),requestVehicle.brand(),requestVehicle.model(),requestVehicle.yearOfManufacture(),requestVehicle.color(),requestVehicle.mileage(),requestVehicle.fuelType(),requestVehicle.dateOfPurchase(),requestVehicle.vehicleCondition());
          VehicleModel vehicleCreated=vehicleRepo.save(vehicle);
          return new ResponseVehicleDTO(vehicleCreated.getId(),vehicleCreated.getOwnerId(),vehicleCreated.getChassisNumber(),vehicleCreated.getRegistrationNumber(),vehicleCreated.getBrand(),vehicleCreated.getModel(),vehicleCreated.getYearOfManufacture(),vehicleCreated.getColor(),vehicleCreated.getMileage(),vehicleCreated.getFuelType(),vehicleCreated.getDateOfPurchase(),vehicleCreated.getVehicleCondition());
 
