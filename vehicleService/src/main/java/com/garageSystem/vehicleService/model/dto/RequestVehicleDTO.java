85a57bb07e0865960ec5823625ad9c0b2a42b7bd
elkadirizouhra <zahiraelkadiri6@gmail.com>
2024-10-27 22:11:48 +0100

    clientServiceAdded

--- vehicleService/src/main/java/com/garageSystem/vehicleService/model/dto/RequestVehicleDTO.java
+++ vehicleService/src/main/java/com/garageSystem/vehicleService/model/dto/RequestVehicleDTO.java
@@ -3,6 +3,6 @@ package com.garageSystem.vehicleService.model.dto;
 import com.garageSystem.vehicleService.model.entity.VehicleCondition;
 
 import java.util.Date;
-public record RequestVehicleDTO(String ownerId, String chassisNumber, String registrationNumber,String brand, String model, Date yearOfManufacture, String color, int mileage, String fuelType, Date dateOfPurchase, VehicleCondition vehicleCondition) {
+public record RequestVehicleDTO(String ownerIdentityNumber, String chassisNumber, String registrationNumber,String brand, String model, Date yearOfManufacture, String color, int mileage, String fuelType, Date dateOfPurchase, VehicleCondition vehicleCondition) {
 
 }
